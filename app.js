2c8342f95dcf9db9bfead5feb7265b5fbd9ee0d0
Sana Khalid <sanabibi.khalid@gmail.com>
2021-01-24 19:29:11 -0800

    updated makeGrid function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.container');
 const refresh = document.querySelector('.refresh');
-const squareSize = document.querySelector('.squareSize')
+const squareSize = document.querySelector('.squareSize');
+
 
 
 function makeGrid(num) {
@@ -9,56 +10,57 @@ function makeGrid(num) {
         const div = document.createElement('div');
         div.className = "square";
         container.append(div);
+
+        //determine the size of the squares
+        let length = 640 / num;
+        div.style.width = `${length}px`;
+        div.style.height = `${length}px`;
+
+
+        div.addEventListener('mouseover', function () {
+            div.style.backgroundColor = 'black';
+        })
+
     }
+
 }
 
 makeGrid(16);
 const square = document.querySelectorAll('.square');
 
-function gridSize(num) {
-    //determine the size of the squares
-    let length = 640 / num;
-    square.forEach((squares) => {
-        squares.style.width = `${length}px`;
-        squares.style.height = `${length}px`;
-    })
-}
+// function styleGrid(color) {
+//     //style the squares on mouseover
+//     square.forEach((squares) => {
+//         squares.addEventListener('mouseover', function () {
+//             squares.style.backgroundColor = color;
+//         })
+//     })
+// }
 
-function styleGrid(color) {
-    //style the squares on mouseover
-    square.forEach((squares) => {
-        squares.addEventListener('mouseover', function () {
-            squares.style.backgroundColor = color;
-        })
-    })
-}
+// styleGrid('black');
 
-function removeSquares() {
-    square.forEach((squares) => {
-        squares.remove()
-    });
+function removeChild() {
+    while (container.firstChild) { container.removeChild(container.firstChild) };
 }
-
-gridSize(16);
-styleGrid('black');
 
 //reset the grid 
 refresh.addEventListener('click', function () {
-    square.forEach((squares) => {
-        squares.style.backgroundColor = 'white';
-    });
-    styleGrid('black');
+    // square.forEach((squares) => {
+    //     squares.style.backgroundColor = 'white';
+    // });
+    // styleGrid('black');
+    removeChild();
+    makeGrid(16);
 })
 
 squareSize.addEventListener('click', function () {
     let result = parseInt(prompt('How many squares per side? Max num = 100!'));
     if (result <= 100) {
-        while (container.firstChild) { container.removeChild(container.firstChild) };
+        removeChild();
+        makeGrid(result);
     }
-    makeGrid(result);
-    gridSize(result);
-    styleGrid('black');
-})
+});
+
 
 
 
@@ -69,3 +71,20 @@ squareSize.addEventListener('click', function () {
 //         makeGrid(result);
 //     }
 // })
+
+
+// function gridSize(num) {
+//     //determine the size of the squares
+//     let length = 640 / num;
+//     square.forEach((squares) => {
+//         squares.style.width = `${length}px`;
+//         squares.style.height = `${length}px`;
+//     })
+// }
+
+
+// function removeSquares() {
+//     square.forEach((squares) => {
+//         squares.remove()
+//     });
+// }
